9782d6c1680f86162bd2beff1c3ec6bb88cd795d
Synz0331 <eeakins06@gmail.com>
2015-03-19 18:29:32 -0600

    Start page, survey page with pictures and comments working.  No idea how to submit data properly

--- contents/pictures/index.js
+++ contents/pictures/index.js
@@ -4,13 +4,13 @@ var picture = {
 
 
         $.get("data/cyberbullying_data2.json.data", function(data) {
-           var result = JSON.parse(data)
 
+            var result = JSON.parse(data)
             //make data arrays so they can be more easily parsed by jade?
 
             $.get("pictures/list.jade", function(template) {
                 var html = jade.render(template, {
-                    data: result
+                    data: result.data
                 })
                 $("#pictures").html(html)
             })
