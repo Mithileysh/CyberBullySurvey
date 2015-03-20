2aaed59f3f8aef0b6612a0354275ee89f828ae02
Synz0331 <eeakins06@gmail.com>
2015-03-20 14:59:36 -0600

    90% done.  Skeleton code is set to store the data for each picture

--- contents/pictures/index.js
+++ contents/pictures/index.js
@@ -1,22 +1,52 @@
 var picture = {
+    complete: function()
+    {
+        $.get("pictures/complete.jade", function(template) 
+        {
+            var html = jade.render(template)
+            $("#pictures").html(html)
+        })
+    },
 
-    loadQuestions: function() {
-
-
+    loadQuestions: function(next,id,e1,e2,e3,e3) {
+        $("#pictures").html()
+        id = id + 1
+        if (next == true)
+        {
+            if (e1 == "true")
+            {
+                if (e3 == "true")
+                {}
+                else
+                {}
+            }
+            else
+            {
+                if (e3 == "true")
+                {}
+                else
+                {}
+            }
+        }
+        if (id == 88)
+        {
+            complete()
+            return
+        }
         $.get("data/cyberbullying_data2.json.data", function(data) {
-
             var result = JSON.parse(data)
             //make data arrays so they can be more easily parsed by jade?
 
             $.get("pictures/list.jade", function(template) {
                 var html = jade.render(template, {
-                    data: result.data
+                    data: result.data[id]
                 })
                 $("#pictures").html(html)
             })
 
         })
     },
+
     load: function() {
 
         $.get("pictures/ui.jade", function(template) {
